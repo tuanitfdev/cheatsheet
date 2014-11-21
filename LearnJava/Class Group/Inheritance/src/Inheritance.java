5476d30bef449537f8ce32944ab81f729598b219
Tuan <tuancnttsp@gmail.com>
2014-11-21 20:00:20 +0700

    Update Inheritance example

--- LearnJava/Class Group/Inheritance/src/Inheritance.java
+++ LearnJava/Class Group/Inheritance/src/Inheritance.java
@@ -5,7 +5,11 @@ public class Inheritance {
 		// TODO Auto-generated method stub
 
 		Bo b = new Bo();
-		b.An();
+		b.An(); // Call overridden method of sub class instance
+		
+		System.out.println("////////////////////////////////////");
+		
+		b.Keu(); // Call method (defined in parent class) of sub class instance
 
 	}
 
